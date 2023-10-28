2dbcef9758d21dd85908cc56c2ccc06374bd9fab
Sufail Badusha <sufailbadushat@gmail.com>
2023-10-28 16:36:03 +0530

    first commit

---
+++ src/main/java/com/assignment/diffrenz/DiffrenzApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.diffrenz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffrenzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiffrenzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/diffrenz/DiffrenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.diffrenz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffrenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
