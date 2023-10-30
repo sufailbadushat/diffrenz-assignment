05eee95804efba1faf5108d43f5d58e71e5f610b
Sufail Badusha <sufailbadushat@gmail.com>
2023-10-30 23:03:13 +0530

    session management

--- src/main/java/com/assignment/diffrenz/config/WebSecurityConfig.java
+++ src/main/java/com/assignment/diffrenz/config/WebSecurityConfig.java
@@ -26,15 +26,9 @@ public class WebSecurityConfig {
     //Authentication
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("admin"))
-                .roles("ADMIN", "USER")
-                .build();
+        UserDetails admin = User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN", "USER").build();
 
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("user"))
-                .roles("USER")
-                .build();
+        UserDetails user = User.withUsername("user").password(encoder.encode("user")).roles("USER").build();
 
         return new InMemoryUserDetailsManager(admin, user);
     }
@@ -42,18 +36,28 @@ public class WebSecurityConfig {
     //Authorization
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> {
                     //auth.requestMatchers("/api").permitAll();
                     auth.requestMatchers("/api/user/**").hasRole("USER");
                     auth.requestMatchers("/api/admin/**").hasRole("ADMIN");
                     auth.anyRequest().authenticated();
                 })
-                .httpBasic(withDefaults())
-                .formLogin(log ->
-                        log.defaultSuccessUrl("/api"))
-                .exceptionHandling(ex ->
-                        ex.accessDeniedPage("/api/access-denied"));
+                .httpBasic(withDefaults()).formLogin(log -> log.defaultSuccessUrl("/api"))
+                .exceptionHandling(ex -> ex.accessDeniedPage("/api/access-denied"))
+                .sessionManagement(session ->
+                        session .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                                .invalidSessionUrl("/login")
+                                .sessionFixation().none()
+                                .sessionAuthenticationErrorUrl("/login")
+                                .maximumSessions(2)
+                                .maxSessionsPreventsLogin(true)
+                                .expiredUrl("/login?invalid-session=true")
+                )
+                .logout(withDefaults())
+        ;
+
+
+        return http.build();
 
 //                        ex.accessDeniedHandler((request, response, accessDeniedException) -> {
 //                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
@@ -61,7 +65,7 @@ public class WebSecurityConfig {
 //                            response.getWriter().write("Access Denied: You do not have the necessary role to access this resource.");
 //                        }))
 
-        return http.build();
+
     }
 
     @Bean
