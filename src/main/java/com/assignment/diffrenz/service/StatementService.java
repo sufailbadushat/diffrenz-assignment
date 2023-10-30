337f614eeed01c5860352f86c7560d2821c45418
Sufail Badusha <sufailbadushat@gmail.com>
2023-10-30 23:44:22 +0530

    hashing added

--- src/main/java/com/assignment/diffrenz/service/StatementService.java
+++ src/main/java/com/assignment/diffrenz/service/StatementService.java
@@ -22,22 +22,20 @@ public class StatementService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private HashService hashService;
 
 
     public List<AccountDtoResponse> getOnAccountId(Long id) throws DataNotFoundException {
         Optional<Account> ac = accountRepository.findById(id);
 
         if (ac.isPresent()) {
-            return ac.stream()
-                    .map(this::accountDTOConverter)
+            return ac.stream().map(this::accountDTOConverterForAdmin)
                     .collect(Collectors.toList());
-        }else {
-            throw new DataNotFoundException("Data not found with id "+ id);
+        } else {
+            throw new DataNotFoundException("Data not found with id " + id);
 
         }
-
-
-
     }
 
 
@@ -47,55 +45,62 @@ public class StatementService {
             throw new DataNotFoundException("No data found");
         }
         return accounts.stream()
-                .map(this::accountDTOConverter)
+                .map(this::accountDTOConverterForUser)
                 .collect(Collectors.toList());
     }
 
     public List<AccountDtoResponse> getBetweenDates(DateRangeStatementAccountDTO dateRange) throws DataNotFoundException {
 
-        List<Account> list = accountRepository.findBetweenDates(dateRange.getId(), dateRange.getFromDate(),
+        List<Account> list = accountRepository.findBetweenDates(dateRange.getId(),
+                dateRange.getFromDate(),
                 dateRange.getToDate());
         if (list.isEmpty()) {
             throw new DataNotFoundException("No data found!");
         }
 
         return list.stream()
-                .map(this::accountDTOConverter)
+                .map(this::accountDTOConverterForAdmin)
                 .collect(Collectors.toList());
     }
 
     public List<AccountDtoResponse> getBetweenAmount(AmountRangeStatementAccount amountRange) throws DataNotFoundException {
 
-        List<Account> accounts = accountRepository.findBetweenAmounts(amountRange.getAccountId(),
-                amountRange.getFromAmount(),
-                amountRange.getToAmount());
+        List<Account> accounts = accountRepository.findBetweenAmounts(amountRange.getAccountId(), amountRange.getFromAmount(), amountRange.getToAmount());
         if (accounts.isEmpty()) {
             throw new DataNotFoundException("No data found!");
         }
-
-
-        return accounts
-                .stream().map(this::accountDTOConverter)
+        return accounts.stream()
+                .map(this::accountDTOConverterForAdmin)
                 .collect(Collectors.toList());
     }
 
 
-
-
-
-
-//Convert individual Account and Statement to Dto
-    private AccountDtoResponse accountDTOConverter(Account account) {
+    //Convert individual Account and Statement to Dto for admin
+    private AccountDtoResponse accountDTOConverterForAdmin(Account account) {
 
         AccountDtoResponse dtoAcc = modelMapper.map(account, AccountDtoResponse.class);
-        List<StatementDtoResponse> dtoState = account.getStatements()
-                .stream()
-                .map(statement -> modelMapper.map(statement, StatementDtoResponse.class))
-                .collect(Collectors.toList());
+
+        List<StatementDtoResponse> dtoState = account.getStatements().stream()
+                .map(statement ->
+                        modelMapper.map(statement, StatementDtoResponse.class))
+                        .collect(Collectors.toList());
         dtoAcc.setStatements(dtoState);
         return dtoAcc;
     }
 
+
+    //Convert individual Account and Statement to Dto for user
+
+    private AccountDtoResponse accountDTOConverterForUser(Account account) {
+
+        AccountDtoResponse dtoAcc = modelMapper.map(account, AccountDtoResponse.class);
+        //Hashing the account number
+        dtoAcc.setAccountNumber(hashService.hashString(dtoAcc.getAccountNumber()));
+
+        List<StatementDtoResponse> dtoState = account.getStatements().stream().map(statement -> modelMapper.map(statement, StatementDtoResponse.class)).collect(Collectors.toList());
+        dtoAcc.setStatements(dtoState);
+        return dtoAcc;
+    }
 
 
 }
